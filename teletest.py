03091e1c9d59db85a02907f3aad086415adeafa9
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2023-04-02 06:31:29 +0800

    Deleted teletest.py

--- teletest.py
+++
@@ -1,6 +0,0 @@
-@dp.callback_query_handler(text=("yes", "no"), state=Form.incl_baggage)
-async def oneway_baggage_query(query: types.CallbackQuery, state: FSMContext):
-    async with state.proxy() as data:
-        data["incl_baggage"] = query.data
-    await Form.search_direct_flight_qn.set()
-    await message.answer("Search for direct flights only?", reply_markup=keyboard_yes_no)
